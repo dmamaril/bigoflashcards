82890d0da6102722e16d7551c7d838b70d539a5a
Joey Yang <joey@joeyyang.com>
2014-02-06 16:53:27 -0800

    flip button now unhides answer

--- js/views/flashcards/index.js
+++ js/views/flashcards/index.js
@@ -11,23 +11,29 @@ define([
     template: template,
     events: {
       "click a.previous": "previous",
-      "click a.next": "next"
+      "click a.next": "next",
+      "click a.flip": "flip"
     },
     previous: function(e){
       e.preventDefault();
-      // router.navigate('/previous');
       if (this.model.id === 0) {
         this.model = this.collection.get(this.collection.length - 1);
       } else this.model = this.collection.get(this.model.id - 1);
       this.render();
     },
+
     next: function(e){
       e.preventDefault();
-      // router.navigate('/next');
       if (this.model.id === this.collection.length - 1) {
         this.model = this.collection.get(0);
       } else this.model = this.collection.get(this.model.id + 1);
       this.render();
+    },
+    
+    flip: function(e){
+      e.preventDefault();
+      $('.hide').toggleClass('hide');
     }
+
   });
 });
