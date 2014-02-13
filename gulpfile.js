a2141a9f8ca0c1e5ce9ec4d3208b23a0d30ed555
Joey Yang <joey@joeyyang.com>
2014-02-12 21:08:01 -0800

    add nodemon gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var changed = require('gulp-changed');
 var stylus = require('gulp-stylus');
+var nodemon = require('gulp-nodemon');
 
 var paths = {
+  all: ['css/**/*.styl', 'js/**/*.js', 'server/**/*.js'],
   input: {
     scripts: ['js/**/*.js'],
     css: ['css/**/*.styl']
@@ -16,17 +18,28 @@ var paths = {
 
 gulp.task('jshint', function(){
   gulp.src(paths.input.scripts)
+      .pipe(changed(paths.output.scripts))
       .pipe(jshint())
       .pipe(jshint.reporter('default'));
 });
 
 gulp.task('changed', function(){
   gulp.src(paths.input.scripts)
-      .pipe(changed());
+      .pipe(changed(paths.output.scripts))
+      .pipe(gulp.dest(paths.output.scripts));
 });
 
 gulp.task('stylus', function(){
-  gulp.src(paths.css)
+  gulp.src(paths.input.css)
+      .pipe(changed(paths.output.css))
       .pipe(stylus())
       .pipe(gulp.dest(paths.output.css));
 });
+
+gulp.task('develop', function(){
+  nodemon({
+    script: 'server.js',
+  }).on('restart', ['jshint', 'stylus']);
+});
+
+// gulp.task('default', ['nodemon']
