8095f5c20fec812c2073c88231a74a187f3eb67f
Joey Yang <joey@joeyyang.com>
2014-02-12 20:56:44 -0800

    write jshint, stylus gulp tasks, begin changed task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var changed = require('gulp-changed');
+var stylus = require('gulp-stylus');
 
 var paths = {
-  scripts: ['js/**/*.js']
+  input: {
+    scripts: ['js/**/*.js'],
+    css: ['css/**/*.styl']
+  },
+  output: {
+    scripts: 'public/js',
+    css: 'public/css'
+  }
 };
 
 gulp.task('jshint', function(){
-  gulp.src(paths.scripts)
+  gulp.src(paths.input.scripts)
       .pipe(jshint())
       .pipe(jshint.reporter('default'));
+});
+
+gulp.task('changed', function(){
+  gulp.src(paths.input.scripts)
+      .pipe(changed());
+});
+
+gulp.task('stylus', function(){
+  gulp.src(paths.css)
+      .pipe(stylus())
+      .pipe(gulp.dest(paths.output.css));
 });
